03c8ca6e64c05e91d8c4bd571824e7a67cfe559c
ronkashi <kashiron1@gmail.com>
2016-12-14 15:47:14 +0200

    files from moodle

---
+++ unit_tests/sp_point_unit_test.c
@@ -0,0 +1,41 @@
+#include "../SPPoint.h"
+#include "unit_test_util.h"
+#include <stdbool.h>
+
+//Checks if copy Works
+bool pointBasicCopyTest() {
+	double data[2] = { 1.0, 1.0 };
+	int dim = 2;
+	int index = 1;
+	SPPoint* p = spPointCreate(data, dim, index);
+	SPPoint* q = spPointCopy(p);
+	ASSERT_TRUE(spPointGetIndex(p) == spPointGetIndex(q));
+	ASSERT_TRUE(spPointGetDimension(p) == spPointGetDimension(q));
+	for (int i = 0; i < spPointGetDimension(p); i++) {
+		ASSERT_TRUE(spPointGetAxisCoor(p, i) == spPointGetAxisCoor(q, i));
+	}
+	spPointDestroy(p);
+	spPointDestroy(q);
+	return true;
+}
+bool pointBasicL2Distance() {
+	double data1[2] = { 1.0, 1.0 };
+	double data2[2] = { 1.0, 0.0 };
+	int dim1 = 2;
+	int dim2 = 2;
+	int index1 = 1;
+	int index2 = 1;
+	SPPoint* p = spPointCreate(data1, dim1, index1);
+	SPPoint* q = spPointCreate(data2, dim2, index2);
+	ASSERT_TRUE(spPointL2SquaredDistance(p,p) == 0.0);
+	ASSERT_TRUE(spPointL2SquaredDistance(q,q) == 0.0);
+	ASSERT_FALSE(spPointL2SquaredDistance(p,q) == 0.0);
+	spPointDestroy(p);
+	spPointDestroy(q);
+	return true;
+}
+int main() {
+	RUN_TEST(pointBasicCopyTest);
+	RUN_TEST(pointBasicL2Distance);
+	return 0;
+}
