d70d024923ffb71422900344ce8b31ce7c9bb43a
ronkashi <kashiron1@gmail.com>
2016-12-30 12:33:40 +0200

    Add files via upload

--- unit_tests/sp_bpriority_queue_unit_test.c
+++ unit_tests/sp_bpriority_queue_unit_test.c
@@ -7,56 +7,111 @@
 //Checks if copy Works
 bool queueBasicCopyTest() {
 	int maxSize = 4;
-	int dataIndex[] = {5,2,3,7};
-	double dataValue[] = {80,40,1.444,3};
+	int dataIndex[] = { 5, 2, 3, 7 };
+	double dataValue[] = { 80, 40, 10, 3 };
 	SPBPQueue* myQueue;
 	BPQueueElement* elmentPoin1;
 	BPQueueElement* elmentPoin2;
 	SPBPQueue* copy;
-	elmentPoin1=(BPQueueElement*)malloc(sizeof(BPQueueElement));
+	elmentPoin1 = (BPQueueElement*) malloc(sizeof(BPQueueElement));
 	assert(NULL != elmentPoin1);
-	elmentPoin2=(BPQueueElement*)malloc(sizeof(BPQueueElement));
+	elmentPoin2 = (BPQueueElement*) malloc(sizeof(BPQueueElement));
 	assert(NULL != elmentPoin2);
 
-	myQueue= spBPQueueCreate(maxSize);
+	myQueue = spBPQueueCreate(maxSize);
 
-	for (int i = 0; i < sizeof(*dataIndex); i++) {
-		spBPQueueEnqueue(myQueue,dataIndex[i],dataValue[i]);
+	for (int i = 0; i < (sizeof(dataIndex) / sizeof(*dataIndex)); i++) {
+		spBPQueueEnqueue(myQueue, dataIndex[i], dataValue[i]);
 	}
 
 	copy = spBPQueueCopy(myQueue);
 	ASSERT_TRUE(spBPQueueGetMaxSize(myQueue) == spBPQueueGetMaxSize(copy));
 	ASSERT_TRUE(spBPQueueSize(myQueue) == spBPQueueSize(copy));
 	for (int i = 0; i < maxSize; i++) {
-		spBPQueuePeek(myQueue,elmentPoin1);
-		spBPQueuePeek(copy,elmentPoin2);
-		ASSERT_TRUE(elmentPoin1->index==elmentPoin2->index);
-		ASSERT_TRUE(elmentPoin1->value==elmentPoin2->value);
+		spBPQueuePeek(myQueue, elmentPoin1);
+		spBPQueuePeek(copy, elmentPoin2);
+		ASSERT_TRUE(elmentPoin1->index == elmentPoin2->index);
+		ASSERT_TRUE(elmentPoin1->value == elmentPoin2->value);
 		spBPQueueDequeue(myQueue);
 		spBPQueueDequeue(copy);
+
 	}
 	spBPQueueDestroy(myQueue);
 	spBPQueueDestroy(copy);
+	free(elmentPoin1);
+	free(elmentPoin2);
 	return true;
 }
-bool pointBasicL2Distance() {
-//	double data1[2] = { 1.0, 1.0 };
-//	double data2[2] = { 1.0, 0.0 };
-//	int dim1 = 2;
-//	int dim2 = 2;
-//	int index1 = 1;
-//	int index2 = 1;
-//	SPPoint* p = spPointCreate(data1, dim1, index1);
-//	SPPoint* q = spPointCreate(data2, dim2, index2);
-//	ASSERT_TRUE(spPointL2SquaredDistance(p,p) == 0.0);
-//	ASSERT_TRUE(spPointL2SquaredDistance(q,q) == 0.0);
-//	ASSERT_FALSE(spPointL2SquaredDistance(p,q) == 0.0);
-//	spPointDestroy(p);
-//	spPointDestroy(q);
+//bounding check
+bool queueBasicBound() {
+	int maxSize = 4;
+	int dataIndex[] = { 5, 2, 3, 7, 8, 9, 10 };
+	double dataValue[] = { 80, 40, 10, 3, 1.4, 1.6, 90 };
+	int sortIndex[] = { 8, 9, 7, 3 };
+	double sortValue[] = { 1.4, 1.6, 3, 10 };
+	SPBPQueue* myQueue;
+	BPQueueElement* elmentPoin1;
+	elmentPoin1 = (BPQueueElement*) malloc(sizeof(BPQueueElement));
+	assert(NULL != elmentPoin1);
+
+	myQueue = spBPQueueCreate(maxSize);
+
+	for (int i = 0; i < (sizeof(dataIndex) / sizeof(*dataIndex)); i++) {
+		spBPQueueEnqueue(myQueue, dataIndex[i], dataValue[i]);
+	}
+
+	ASSERT_TRUE(spBPQueueGetMaxSize(myQueue) == 4);
+	ASSERT_TRUE(spBPQueueSize(myQueue) == 4);
+	ASSERT_TRUE(spBPQueueMaxValue(myQueue) == 10);
+	ASSERT_TRUE(spBPQueueMinValue(myQueue) == 1.4);
+	ASSERT_TRUE(spBPQueueIsFull(myQueue));
+	ASSERT_FALSE(spBPQueueIsEmpty(myQueue));
+
+	for (int i = 0; i < maxSize; i++) {
+		spBPQueuePeek(myQueue, elmentPoin1);
+		ASSERT_TRUE(elmentPoin1->index == sortIndex[i]);
+		ASSERT_TRUE(elmentPoin1->value == sortValue[i]);
+		spBPQueueDequeue(myQueue);
+	}
+	ASSERT_FALSE(spBPQueueIsFull(myQueue));
+	ASSERT_TRUE(spBPQueueIsEmpty(myQueue));
+
+	free(elmentPoin1);
+	spBPQueueDestroy(myQueue);
+	return true;
+}
+
+//clearing check
+bool queueBasicClear() {
+	int maxSize = 4;
+	int dataIndex[] = { 5, 2, 3, 7, 8, 9, 10 };
+	double dataValue[] = { 80, 40, 10, 3, 1.4, 1.6, 90 };
+	SPBPQueue* myQueue;
+	BPQueueElement* elmentPoin1;
+	elmentPoin1 = (BPQueueElement*) malloc(sizeof(BPQueueElement));
+	assert(NULL != elmentPoin1);
+
+	myQueue = spBPQueueCreate(maxSize);
+
+	for (int i = 0; i < (sizeof(dataIndex) / sizeof(*dataIndex)); i++) {
+		spBPQueueEnqueue(myQueue, dataIndex[i], dataValue[i]);
+	}
+
+	spBPQueueClear(myQueue);
+	ASSERT_TRUE(spBPQueueGetMaxSize(myQueue) == 4);
+	ASSERT_TRUE(spBPQueueSize(myQueue) == 0);
+	ASSERT_TRUE(spBPQueueMaxValue(myQueue) < 0);
+	ASSERT_TRUE(spBPQueueMinValue(myQueue) < 0);
+	ASSERT_FALSE(spBPQueueIsFull(myQueue));
+	ASSERT_TRUE(spBPQueueIsEmpty(myQueue));
+
+	free(elmentPoin1);
+	spBPQueueDestroy(myQueue);
 	return true;
 }
 int main() {
 	RUN_TEST(queueBasicCopyTest);
-	RUN_TEST(pointBasicL2Distance);
+	RUN_TEST(queueBasicBound);
+	RUN_TEST(queueBasicClear);
 	return 0;
 }
