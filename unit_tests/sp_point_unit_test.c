2ff795ee568ebffe0c5fce2d37e25959b905d50a
gavrielc <gabicohen22@yahoo.com>
2017-01-01 22:11:26 +0200

    Adding third test

--- unit_tests/sp_point_unit_test.c
+++ unit_tests/sp_point_unit_test.c
@@ -34,8 +34,32 @@ bool pointBasicL2Distance() {
 	spPointDestroy(q);
 	return true;
 }
+bool pointAdvancedMixedTest () {
+	double data1[10] = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0 };
+	double data2[10] = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 11.0 };
+	int dim1 = 10;
+	int dim2 = 10;
+	int index1 = 3;
+	int index2 = 7;
+	SPPoint* p = spPointCreate(data1, dim1, index1);
+	SPPoint* q = spPointCreate(data2, dim2, index2);
+	SPPoint* r = spPointCopy(p);
+	ASSERT_TRUE(spPointL2SquaredDistance(p,p) == 0.0);
+	ASSERT_TRUE(spPointL2SquaredDistance(q,q) == 0.0);
+	ASSERT_TRUE(spPointL2SquaredDistance(p,q) == 1.0);
+	ASSERT_TRUE(spPointGetIndex(p) == spPointGetIndex(r));
+	ASSERT_TRUE(spPointGetDimension(p) == spPointGetDimension(r));
+	for (int i = 0; i < spPointGetDimension(p); i++) {
+		ASSERT_TRUE(spPointGetAxisCoor(p, i) == spPointGetAxisCoor(r, i));
+	}
+	spPointDestroy(p);
+	spPointDestroy(q);
+	spPointDestroy(r);
+	return true;
+}
 int main() {
 	RUN_TEST(pointBasicCopyTest);
 	RUN_TEST(pointBasicL2Distance);
+	RUN_TEST(pointAdvancedMixedTest);
 	return 0;
 }
