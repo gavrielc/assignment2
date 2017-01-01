42954bec2914eb1da9072aee2b94386af17a872b
gavrielc <gabicohen22@yahoo.com>
2017-01-01 17:20:16 +0200

    Update main.c with comments

--- main.c
+++ main.c
@@ -1,23 +1,23 @@
 #include "main_aux.h"
 
+/**
+ * Initially recieves three integers from stdin, n,k,d. The receives n points of dimension d and
+ * a comparison point of the same dimension. 
+ * Prints to stdout the indices of the k points that are closest to the comparison point 
+ * based on L2SquaredDistance
+ *
+ */
 int main() {
 	int n, d, k;
 	SPPoint** pointArray;
 	SPBPQueue* queue;
-	scanf("%d %d %d%*c", &n, &d, &k);
-	fflush(NULL);
-	pointArray = fillPointArray(n, d);
-	printf("filled point array\n");
-	fflush(NULL);
+	scanf("%d %d %d%*c", &n, &d, &k);  // gets three integers and discards the newline  
+	pointArray = fillPointArray(n, d);  // fills an array with the n points and comparison point from stdin input
 	if(pointArray == NULL) {
 		printf("Memory allocation failure.");
 		return 0;
 	}
-	queue = fillQueue(pointArray, n, k);
-	printf("filled queue\n");
-	fflush(NULL);
-	printIndices(queue, k);
-	printf("printed indices\n");
-	fflush(NULL);
+	queue = fillQueue(pointArray, n, k); // fills an SPB Priority queue with the indices and distances
+	printIndices(queue, k); // prints to stdout the indices in the correct order
 	return 0;
 }
