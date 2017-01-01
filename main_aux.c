0854061de29fd02d97c9b4bb381d9e9301178b2b
gavrielc <gabicohen22@yahoo.com>
2017-01-01 17:30:32 +0200

    Moving function descriptions to header file

--- main_aux.c
+++ main_aux.c
@@ -1,17 +1,6 @@
 #include "main_aux.h"
 
 
-/**
- * Fills an new array of Points with points that are created 
- * from user input numbers.
- *
- *
- * @param n is the number of points to compare
- * @param dimension is the dimension of each point
- * @return
- * NULL in case allocation failure occurred
- * Otherwise, the new point Array is returned
- */
 SPPoint** fillPointArray(int n, int dimension) {
 	int i, j;
 	double* cord_array;
@@ -51,18 +40,6 @@ SPPoint** fillPointArray(int n, int dimension) {
 }
 
 
-/**
- * Fills a new queue with elements where the index is the point index and the value
- * is the L2 Squared distance from the comparison point 
- *
- *
- * @param pointArray is the Array containing all of the points to insert
- * @param n is the number of points to insert
- * @param k is the maxsize of the queue
- * @return
- * NULL in case allocation failure occurred
- * Otherwise, the new queue is returned
- */
 SPBPQueue* fillQueue(SPPoint** pointArray, int n, int k) {
 	int i;
 	SPBPQueue* queue;
@@ -79,15 +56,7 @@ SPBPQueue* fillQueue(SPPoint** pointArray, int n, int k) {
 	return queue;
 }
 
-/**
- * Prints the index of each of the elements in the queue to stdout. 
- * They are printed in the order of the smallest value to the largest. The index breaks ties in value.
- *
- *
- * @param queue is the queue containing all of the points to print
- * @param k is the maxsize of the queue
- * 
- */
+
 void printIndices(SPBPQueue* queue, int k) {
 	BPQueueElement* element;
 	int i;
